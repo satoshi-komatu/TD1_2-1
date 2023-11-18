41f263316dabe0bd66206c8e7d3fc081894070b8
K023G1066@g.neec.ac.jp <K023G1066@g.neec.ac.jp>
2023-11-18 10:21:29 +0900

    ブロックの当たり判定、スクロール、ジャンプを追加

--- main.cpp
+++ main.cpp
@@ -1,105 +1,158 @@
 #include <Novice.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
-const char kWindowTitle[] = "学籍番号";
+const char kWindowTitle[] = "subject";
+
+// 構造体
+// 座標とスピード
+typedef struct Vector2 {
+	int x;
+	int y;
+	int speed;
+} Vector2;
 
 typedef struct Vector2f {
 	float x;
 	float y;
 } Vector2f;
 
-struct Vector2 {
-	int x = 0;
-	int y = 0;
-};
-
 typedef struct Ball {
-	Vector2f position = { 680.0f, 50.0f };
-	Vector2f velocity = { 0.0f, 0.0f };
-	Vector2f acceleration = { 0.0f, -0.8f };
-	int radius = 30;
-	unsigned int color = WHITE;
+	Vector2f velocity = {5.0f, 5.0f};
+	Vector2f acceleration = {0.0f, 1.0f};
 } Ball;
 
 Ball ball;
+
+// ステージ
+enum STAGE { TITLE, YESJAMP, MENU };
+
+STAGE stage = TITLE;
+
+// player座標
+Vector2 playerPos = {50, 50, 3};
+// playerのスクロール座標
+// 背景のスクロール
+Vector2 scroll;
+
+// 当たり判定
+typedef struct horn {
+	// 左上
+	int leftTopX;
+	int leftTopY;
+	// 左下
+	int leftBottomX;
+	int leftBottomY;
+	// 右上
+	int rightTopX;
+	int rightTopY;
+	// 右下
+	int rightBottomX;
+	int rightBottomY;
+
+} horn;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+	Novice::Initialize(kWindowTitle, 800, 800);
 
 	// キー入力結果を受け取る箱
-	char keys[256] = { 0 };
-	char preKeys[256] = { 0 };
-
-	// player座標
-	Vector2 playerWorld = { 0, 0 };
-	// playerのスクロール座標
-	Vector2 playerScroll;
-	// スピード
-	int playerSpeed = 5;
-	// 背景のスクロール
-	Vector2 backgroundScroll;
-
-	// 背景のスクロール座標
-	Vector2 backScroll[4][4];
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			backScroll[i][j] = { i * 1280, j * 720 };
-		}
-	}
-	// mapのスクロール座標
-	//   map
-	//  タイルサイズ
-	int tile = 70;
-	const int mapX = 32;
-	const int mapY = 9;
-	int map1[mapY][mapX] = {
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 0,
-		 0, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-	};
-	// タイルの種類
-	enum Tile {
-		// 描画なし
-		NOBLOCK,
-		// 足し算
-		ADD,
-		// 引き算
-		SUBTRACTION,
-		// 掛け算
-		MULTIPLICATION,
-		// 割り算
-		DIVISION,
-		// ただのブロック
-		NOMAL
-	};
+	char keys[256] = {0};
+	char preKeys[256] = {0};
 
 	// 絵
-	int backgroundHandle[4][4] = {};
-	backgroundHandle[0][0] = Novice::LoadTexture("./Resource/bg1.png");
-	backgroundHandle[0][1] = Novice::LoadTexture("./Resource/bg2.png");
-	backgroundHandle[0][2] = Novice::LoadTexture("./Resource/bg3.png");
-	backgroundHandle[0][3] = Novice::LoadTexture("./Resource/bg4.png");
-	backgroundHandle[1][0] = Novice::LoadTexture("./Resource/bg2.png");
-	backgroundHandle[1][1] = Novice::LoadTexture("./Resource/bg3.png");
-	backgroundHandle[1][2] = Novice::LoadTexture("./Resource/bg4.png");
-	backgroundHandle[1][3] = Novice::LoadTexture("./Resource/bg1.png");
-	backgroundHandle[2][0] = Novice::LoadTexture("./Resource/bg3.png");
-	backgroundHandle[2][1] = Novice::LoadTexture("./Resource/bg4.png");
-	backgroundHandle[2][2] = Novice::LoadTexture("./Resource/bg1.png");
-	backgroundHandle[2][3] = Novice::LoadTexture("./Resource/bg2.png");
-	backgroundHandle[3][0] = Novice::LoadTexture("./Resource/bg4.png");
-	backgroundHandle[3][1] = Novice::LoadTexture("./Resource/bg1.png");
-	backgroundHandle[3][2] = Novice::LoadTexture("./Resource/bg2.png");
-	backgroundHandle[3][3] = Novice::LoadTexture("./Resource/bg3.png");
+	int titleTexture = Novice::LoadTexture("./Resources./Title.png");
+	int falseStageYesJump = Novice::LoadTexture("./Resources./falseYesJamp.png");
+	int trueStageYesJump = Novice::LoadTexture("./Resources./trueYesJamp.png");
+
+	// マップサイズ
+	const int mapX = 50;
+	const int mapY = 25;
+	// タイルサイズ
+	int mapTileSize = 50;
+	int playerTileSize = 50;
+	// マップ
+	int map[mapY][mapX] = {
+	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+	     0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
+	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
+	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1,
+	     1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+	    {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
+	     1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+	    {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1,
+	     1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+	    {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1,
+	     1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+	    {1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	     0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+	     1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	     0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    };
+
+	// 移動する前にいる座標の変数
+
+	int oldScrollX = scroll.x;
+	int oldScrollY = scroll.y;
+
+	int nowMapX = mapX;
+	int nowMapY = mapY;
+
+	horn box = {
+	    // 左上
+	    (playerPos.x / playerTileSize),
+	    (playerPos.y / playerTileSize),
+	    // 左下
+	    (playerPos.x - playerTileSize) / mapTileSize,
+	    (playerPos.y + playerTileSize - 1) / mapTileSize,
+	    // 右上
+	    (playerPos.x + playerTileSize - 1) / mapTileSize,
+	    (playerPos.y - playerTileSize) / mapTileSize,
+	    // 右下
+	    (playerPos.x + playerTileSize - 1) / mapTileSize,
+	    (playerPos.y + playerTileSize - 1) / mapTileSize,
+	};
+	// ステージ選択
+	bool isYesJamp = false;
+	// ジャンプするかしないか
+	bool isJamp = false;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -114,56 +167,168 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		// マップの更新座標
-		int nowMapX = mapX;
-		// int nowMapY = mapY;
+		switch (stage) {
 
-		// ジャンプ
-		float reflectY = float((playerWorld.y - 700)) * -1;
-		ball.acceleration.y = -0.8f;
+#pragma region TITLE
+		case TITLE:
+			if (keys[DIK_UP] || keys[DIK_W]) {
+				isYesJamp = true;
+			}
+			if (isYesJamp == true && keys[DIK_RETURN]) {
+				stage = YESJAMP;
+			}
+			if (keys[DIK_DOWN] || keys[DIK_S]) {
+				isYesJamp = false;
+			}
+			break;
+#pragma endregion
 
-		if ((keys[DIK_SPACE] && preKeys[DIK_SPACE] == 0) != 0 && playerWorld.y == ball.radius) {
-			ball.velocity.y = 20.0f;
-		}
-
-		ball.velocity.x += ball.acceleration.x;
-		ball.velocity.y += ball.acceleration.y;
-
-		playerWorld.x += int(ball.velocity.x);
-		playerWorld.y += int(ball.velocity.y);
-
-		if (playerWorld.y <= int(ball.radius)) {
-			playerWorld.y = int(ball.radius);
-		}
-
-		// 左
-		if ((keys[DIK_A] || keys[DIK_LEFT]) && playerWorld.x > 0) {
-			if (playerWorld.x >= 640 && playerWorld.x <= 4480) {
-				playerScroll.x -= playerSpeed;
-				backgroundScroll.x -= playerSpeed;
-				nowMapX -= playerSpeed;
-				if (backgroundScroll.x < 0 || playerScroll.x < 0) {
-					backgroundScroll.x = 0;
-					playerScroll.x = 0;
+#pragma region YESJAMP
+			// ジャンプアリ
+		case YESJAMP:
+			// スクロール止める
+			oldScrollX = scroll.x;
+			oldScrollY = scroll.y;
+			// 角の判定
+			box.leftTopX = (playerPos.x) / mapTileSize;
+			box.leftTopY = (playerPos.y) / mapTileSize;
+			box.leftBottomX = (playerPos.x) / mapTileSize;
+			box.leftBottomY = (playerPos.y + playerTileSize - 1) / mapTileSize;
+			box.rightTopX = (playerPos.x + playerTileSize - 1) / mapTileSize;
+			box.rightTopY = (playerPos.y) / mapTileSize;
+			box.rightBottomX = (playerPos.x + playerTileSize - 1) / mapTileSize;
+			box.rightBottomY = (playerPos.y + playerTileSize - 1) / mapTileSize;
+			// ジャンプ
+			// スペースでジャンプ
+			if ((keys[DIK_SPACE] && preKeys[DIK_SPACE] == 0) && isJamp == false) {
+				isJamp = true;
+				ball.velocity.y = -21.0f;
+				// スクロールさせない
+				if (playerPos.y >= 100 && playerPos.y <= 550) {
+					scroll.y = oldScrollY;
 				}
 			}
-			playerWorld.x -= playerSpeed;
-		}
-		// 右
-		if ((keys[DIK_D] || keys[DIK_RIGHT]) && playerWorld.x < 5120) {
-			if (playerWorld.x >= 640 && playerWorld.x <= 4480) {
-				playerScroll.x += playerSpeed;
-				backgroundScroll.x += playerSpeed;
-				nowMapX -= playerSpeed;
-				if (backgroundScroll.x > 3840 || playerScroll.x > 3840) {
-					backgroundScroll.x = 3840;
-					playerScroll.x = 3840;
+			// 速度に加速度加算
+			ball.velocity.y += ball.acceleration.y;
+
+			// 角の当たり判定
+			box.leftTopX = (playerPos.x) / mapTileSize;
+			box.leftTopY = (playerPos.y + int(ball.velocity.y)) / mapTileSize;
+			box.leftBottomX = (playerPos.x) / mapTileSize;
+			box.leftBottomY =
+			    (playerPos.y + playerTileSize - 1 + int(ball.velocity.y)) / mapTileSize;
+			box.rightTopX = (playerPos.x + playerTileSize - 1) / mapTileSize;
+			box.rightTopY = (playerPos.y + int(ball.velocity.y)) / mapTileSize;
+			box.rightBottomX = (playerPos.x + playerTileSize - 1) / mapTileSize;
+			box.rightBottomY =
+			    (playerPos.y + playerTileSize - 1 + int(ball.velocity.y)) / mapTileSize;
+			// ジャンプしたときに地面にのめりこむかのめりこまないか
+			if (map[box.leftTopY][box.leftTopX] == 1 || map[box.rightTopY][box.rightTopX] == 1) {
+				ball.velocity.y = 0;
+				playerPos.y = (box.leftTopY + 1) * mapTileSize;
+			}
+			// 自由落下したときに地面にのめりこむかのめりこまないか
+			if (map[box.leftBottomY][box.leftBottomX] == 1 ||
+			    map[box.rightBottomY][box.rightBottomX] == 1) {
+				isJamp = false;
+				ball.velocity.y = 0;
+				playerPos.y = (box.leftBottomY - 1) * mapTileSize;
+			}
+			// 自由落下中のスクロール
+			// スクロールする
+			if (playerPos.y >= 50 && playerPos.y <= 550) {
+				if (map[box.leftTopY][box.leftTopX] == 0 &&
+				    map[box.leftBottomY][box.leftBottomX] == 0 &&
+				    map[box.rightTopY][box.rightTopX] == 0 &&
+				    map[box.rightBottomY][box.rightBottomX] == 0) {
+					scroll.y += int(ball.velocity.y);
 				}
 			}
-			playerWorld.x += playerSpeed;
-		}
+			// スクロールが止まる
+			if (playerPos.y <= 50) {
+				scroll.y = 0;
+			}
+			// スクロールが止まる
+			if (playerPos.y >= 550) {
+				scroll.y = 450;
+			}
+			// Yに速度加算
+			playerPos.y += int(ball.velocity.y);
 
-		// 当たり判定
+			// マップ座標更新
+			nowMapX = mapX;
+			nowMapY = mapY;
+			// スクロール止める
+			oldScrollX = scroll.x;
+			oldScrollY = scroll.y;
+
+			// キー入力
+			//  左
+			if ((keys[DIK_LEFT] || keys[DIK_A])) {
+				// 左角の判定
+				box.leftTopX = (playerPos.x - int(ball.velocity.x)) / mapTileSize;
+				box.leftTopY = (playerPos.y) / mapTileSize;
+				box.leftBottomX = (playerPos.x - int(ball.velocity.x)) / mapTileSize;
+				box.leftBottomY = (playerPos.y + playerTileSize - 1) / mapTileSize;
+				// 進む先がブロックじゃなかったら左にスピード分進む
+				if (map[box.leftTopY][box.leftTopX] == 0 &&
+				    map[box.leftBottomY][box.leftBottomX] == 0) {
+					playerPos.x -= int(ball.velocity.x);
+					if (playerPos.x >= 350 && playerPos.x < 2050) {
+						scroll.x -= int(ball.velocity.x);
+						// 左上か左下がブロックだったらスクロールが止まる
+						if (map[box.leftTopY][box.leftTopX] == 1 ||
+						    map[box.leftBottomY][box.leftBottomX] == 1) {
+							scroll.x = oldScrollX;
+						}
+					}
+				}
+				// 0だったらスクロールしない(左側）
+				if (playerPos.x <= 350) {
+					scroll.x = 0;
+				}
+				// 1700だったらスクロールしない
+				if (playerPos.x >= 2050) {
+					scroll.x = 1700;
+				}
+			}
+			// 右
+			if ((keys[DIK_RIGHT] || keys[DIK_D])) {
+				// 右角の判定
+				box.rightTopX =
+				    (playerPos.x + playerTileSize - 1 + int(ball.velocity.x)) / mapTileSize;
+				box.rightTopY = (playerPos.y) / mapTileSize;
+				box.rightBottomX =
+				    (playerPos.x + playerTileSize - 1 + int(ball.velocity.x)) / mapTileSize;
+				box.rightBottomY = (playerPos.y + playerTileSize - 1) / mapTileSize;
+				// 進む先がブロックじゃなかったら右にスピード分進む
+				if (map[box.rightTopY][box.rightTopX] == 0 &&
+				    map[box.rightBottomY][box.rightBottomX] == 0) {
+					playerPos.x += int(ball.velocity.x);
+					if (playerPos.x >= 350 && playerPos.x < 2050) {
+						scroll.x += int(ball.velocity.x);
+						// 右上か右下がブロックだったらスクロールが止まる
+						if (map[box.rightTopY][box.rightTopX] == 1 ||
+						    map[box.rightBottomY][box.rightBottomX] == 1) {
+							scroll.x = oldScrollX;
+						}
+					}
+				}
+				// 0だったらスクロールしない（左側）
+				if (playerPos.x <= 350) {
+					scroll.x = 0;
+				}
+				// 1700だったらスクロールしない
+				if (playerPos.x >= 2050) {
+					scroll.x = 1700;
+				}
+			}
+			break;
+#pragma endregion
+
+			// シーン終了
+			break;
+		}
 
 		///
 		/// ↑更新処理ここまで
@@ -173,74 +338,49 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///
 
-		// 絵
+		switch (stage) {
+#pragma region Title
+			// タイトル
+		case TITLE:
+			Novice::DrawSprite(0, 0, titleTexture, 1.0f, 1.0f, 0.0f, WHITE);
 
-		// 背景
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				Novice::DrawSprite(
-					backScroll[i][j].x - backgroundScroll.x,
-					backScroll[i][j].y - backgroundScroll.y, backgroundHandle[i][j], 1, 1, 0.0f,
-					WHITE);
+			Novice::DrawSprite(0, 0, titleTexture, 1.0f, 1.0f, 0.0f, WHITE);
+			if (isYesJamp == false) {
+				Novice::DrawSprite(0, 500, falseStageYesJump, 1.0f, 1.0f, 0.0f, WHITE);
 			}
-		}
-		// ブロック
-		for (int y = 0; y < mapY; y++) {
-			for (int x = 0; x < mapX; x++) {
-				switch (map1[y][x]) {
-					// 描画なし
-				case NOBLOCK:
-					break;
-					// 足し算
-				case ADD:
-					Novice::DrawBox(
-						(x * tile) - backgroundScroll.x, (y * tile) - backgroundScroll.y, tile,
-						tile, 0.0f, RED, kFillModeSolid);
-					break;
-					// 引き算
-				case SUBTRACTION:
-					Novice::DrawBox(
-						(x * tile) - backgroundScroll.x, (y * tile) - backgroundScroll.y, tile,
-						tile, 0.0f, GREEN, kFillModeSolid);
-					break;
-					// 掛け算
-				case MULTIPLICATION:
-					Novice::DrawBox(
-						(x * tile) - backgroundScroll.x, (y * tile) - backgroundScroll.y, tile,
-						tile, 0.0f, BLUE, kFillModeSolid);
-					break;
-					// 割り算
-				case DIVISION:
-					Novice::DrawBox(
-						(x * tile) - backgroundScroll.x, (y * tile) - backgroundScroll.y, tile,
-						tile, 0.0f, BLACK, kFillModeSolid);
-					break;
-					// ただのブロック
-				case NOMAL:
-					Novice::DrawBox(
-						(x * tile) - backgroundScroll.x, (y * tile) - backgroundScroll.y, tile,
-						tile, 0.0f, WHITE, kFillModeSolid);
-					break;
+			if (isYesJamp == true) {
+				Novice::DrawSprite(0, 500, trueStageYesJump, 1.0f, 1.0f, 0.0f, WHITE);
+			}
+			break;
+#pragma endregion
+
+#pragma region YESTITLE
+			// ジャンプあり
+		case YESJAMP:
+			for (int y = 0; y < mapY; y++) {
+				for (int x = 0; x < mapX; x++) {
+					if (map[y][x] == 1) {
+						Novice::DrawBox(
+						    (x * mapTileSize) - scroll.x, (y * mapTileSize) - scroll.y, mapTileSize,
+						    mapTileSize, 0.0f, RED, kFillModeSolid);
+					}
+					if (map[y][x] == 0) {
+						Novice::DrawBox(
+						    (x * mapTileSize) - scroll.x, (y * mapTileSize) - scroll.y, mapTileSize,
+						    mapTileSize, 0.0f, BLACK, kFillModeSolid);
+					}
 				}
 			}
+			// player表記
+			Novice::DrawBox(
+			    (playerPos.x) - scroll.x, (int)(playerPos.y) - scroll.y, playerTileSize,
+			    playerTileSize, 0.0f, BLUE, kFillModeSolid);
+			break;
+#pragma endregion
+
+			// シーン終了
+			break;
 		}
-		// 円
-		Novice::DrawEllipse(
-			playerWorld.x - playerScroll.x, int(reflectY) - playerScroll.y, ball.radius,
-			ball.radius, 0.0f, WHITE, kFillModeSolid);
-
-		Novice::ScreenPrintf(10, 10, "playerWorld(%d, %d)", playerWorld.x, playerWorld.y);
-		Novice::ScreenPrintf(10, 25, "playerScroll(%d, %d)", playerScroll.x, playerScroll.y);
-
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4; j++) {
-				Novice::ScreenPrintf(
-					10 + (300 * i), 50 + (15 * j), "backScroll[%d][%d].x = %d", i, j,
-					backScroll[i][j].x);
-			}
-		}
-
-		Novice::ScreenPrintf(500, 500, "%d", nowMapX);
 
 		///
 		/// ↑描画処理ここまで
